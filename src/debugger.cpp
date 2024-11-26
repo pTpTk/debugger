4ee50edeec5886c6ff60ad1ff46d5b201c0f66a0
pTpTk <zxu44@u.rochester.edu>
2024-11-26 16:44:17 +0800

    print source line on breakpoint

--- src/debugger.cpp
+++ src/debugger.cpp
@@ -21,13 +21,76 @@ namespace {
 }
 
 
+Debugger::Debugger(char* program, pid_t pid) 
+    : name{program}, pid{pid}, regfile(program, pid) {
+
+    auto fd = open(program, O_RDONLY);
+
+    m_elf = elf::elf{elf::create_mmap_loader(fd)};
+    m_dwarf = dwarf::dwarf{dwarf::elf::create_loader(m_elf)};
+
+}
+
 void Debugger::wait_for_signal() {
     int wait_status;
     waitpid(pid, &wait_status, 0);
+
+    siginfo_t siginfo;
+    ptrace(PTRACE_GETSIGINFO, pid, nullptr, &siginfo);
+
+    switch(siginfo.si_signo) {
+        case SIGTRAP:
+            handle_sigtrap(siginfo);
+            break;
+        case SIGSEGV:
+            std::cout << "Segfault. Reason: " << siginfo.si_code << std::endl;
+            break;
+        default:
+            std::cout << "Got signal " << strsignal(siginfo.si_signo) << std::endl;
+    }
+}
+
+void Debugger::handle_sigtrap(siginfo_t info) {
+    switch (info.si_code) {
+    //one of these will be set if a breakpoint was hit
+    case SI_KERNEL:
+    case TRAP_BRKPT:
+    {
+        set_pc(get_pc()-1); //put the pc back where it should be
+        std::cout << "Hit breakpoint at address 0x" << std::hex << get_pc() << std::endl;
+        auto offset_pc = offset_load_addr(get_pc()); //rember to offset the pc for querying DWARF
+        auto line_entry = get_line_entry_from_pc(offset_pc);
+        print_source(line_entry->file->path, line_entry->line);
+        return;
+    }
+    //this will be set if the signal was sent by single stepping
+    case TRAP_TRACE:
+        return;
+    default:
+        std::cout << "Unknown SIGTRAP code " << info.si_code << std::endl;
+        return;
+    }
+}
+
+void Debugger::initialize_load_addr() {
+    if(m_elf.get_hdr().type == elf::et::dyn) {
+        std::ifstream map("/proc/" + std::to_string(pid) + "/maps");
+
+        std::string addr;
+        std::getline(map, addr, '-');
+        addr = "0x" + addr;
+
+        load_addr = std::stol(addr, 0, 16);
+    }
+}
+
+uint64_t Debugger::offset_load_addr(uint64_t addr) {
+   return addr - load_addr;
 }
 
 void Debugger::run() {
     wait_for_signal();
+    initialize_load_addr();
 
     char* line = nullptr;
     while((line = linenoise("dbg> ")) != nullptr) {
@@ -65,13 +128,17 @@ void Debugger::handle_command(const std::string& line) {
     }
 }
 
+std::intptr_t Debugger::get_pc() {
+    return regfile.get_pc();
+}
+
+void Debugger::set_pc(std::intptr_t pc) {
+    reg_write("rip", pc);
+}
+
 void Debugger::continue_execution() {
-
-    std:intptr_t prev_pc = regfile.get_pc() - 1;
-
-    if(auto it = breakpoints.find(prev_pc); it != breakpoints.end()) {
+    if(auto it = breakpoints.find(get_pc()); it != breakpoints.end()) {
         if(it->second.is_enabled()) {
-            reg_write("rip", prev_pc);  // back up pc
             it->second.disable();       // restore inst
             ptrace(PTRACE_SINGLESTEP, pid, nullptr, nullptr);
 
@@ -104,4 +171,73 @@ void Debugger::reg_read(const std::string& reg) {
 
 void Debugger::reg_write(const std::string& reg, uint64_t val) {
     regfile.reg_write(reg, val);
+}
+
+dwarf::die Debugger::get_function_from_pc(uint64_t pc) {
+    for(auto& cu : m_dwarf.compilation_units()) {
+        if(dwarf::die_pc_range(cu.root()).contains(pc)) {
+            for(const auto& die : cu.root()) {
+                if(die.tag == dwarf::DW_TAG::subprogram) {
+                    if(dwarf::die_pc_range(die).contains(pc)) {
+                        return die;
+                    }
+                }
+            }
+        }
+    }
+
+    std::cerr << "can't find function at pc 0x" << std::hex << pc << std::endl;
+    assert(false);
+}
+
+dwarf::line_table::iterator Debugger::get_line_entry_from_pc(uint64_t pc) {
+    for(auto& cu : m_dwarf.compilation_units()) {
+        if(dwarf::die_pc_range(cu.root()).contains(pc)) {
+            auto& lt = cu.get_line_table();
+            auto it = lt.find_address(pc);
+            if (it == lt.end()) {
+                std::cerr << "can't find line entry" << std::endl;
+                assert(false);
+            }
+            else {
+                return it;
+            }
+        }
+    }
+
+    std::cerr << "can't find line entry" << std::endl;
+    assert(false);
+}
+
+void Debugger::print_source(const std::string& file_name, unsigned line, unsigned n_lines_context) {
+    std::ifstream file {file_name};
+
+    //Work out a window around the desired line
+    auto start_line = line <= n_lines_context ? 1 : line - n_lines_context;
+    auto end_line = line + n_lines_context + (line < n_lines_context ? n_lines_context - line : 0) + 1;
+
+    char c{};
+    auto current_line = 1u;
+    //Skip lines up until start_line
+    while (current_line != start_line && file.get(c)) {
+        if (c == '\n') {
+            ++current_line;
+        }
+    }
+
+    //Output cursor if we're at the current line
+    std::cout << (current_line==line ? "> " : "  ");
+
+    //Write lines up until end_line
+    while (current_line <= end_line && file.get(c)) {
+        std::cout << c;
+        if (c == '\n') {
+            ++current_line;
+            //Output cursor if we're at the current line
+            std::cout << (current_line==line ? "> " : "  ");
+        }
+    }
+
+    //Write newline and make sure that the stream is flushed properly
+    std::cout << std::endl;
 }
--- src/debugger.h
+++ src/debugger.h
@@ -9,16 +9,20 @@
 #include <cstdint>
 #include <unordered_map>
 #include <sys/user.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <fstream>
 
 #include "linenoise/linenoise.h"
 #include "breakpoint.h"
 #include "regfile.h"
+#include "libelfin/dwarf/dwarf++.hh"
+#include "libelfin/elf/elf++.hh"
 
 class Debugger
 {
   public:
-    Debugger(char* program, pid_t pid) 
-    : name{program}, pid{pid}, regfile(program, pid) {}
+    Debugger(char* program, pid_t pid);
 
     void run();
 
@@ -27,14 +31,31 @@ class Debugger
     pid_t pid;
     std::unordered_map<std::intptr_t,Breakpoint> breakpoints;
     Regfile regfile;
+    elf::elf m_elf;
+    dwarf::dwarf m_dwarf;
+    std::intptr_t load_addr;
 
     void wait_for_signal();
+    void handle_sigtrap(siginfo_t info);
 
+
+    void initialize_load_addr();
+    uint64_t offset_load_addr(uint64_t addr);
+    
     void handle_command(const std::string& line);
     void continue_execution();
 
+    // breakpoint
     void set_breakpoint(std::intptr_t addr);
+    std::intptr_t get_pc();
+    void set_pc(std::intptr_t pc);
+
+    // register
     void reg_dump();
     void reg_read(const std::string& reg);
     void reg_write(const std::string& reg, uint64_t val);
+    
+    dwarf::die get_function_from_pc(uint64_t pc);
+    dwarf::line_table::iterator get_line_entry_from_pc(uint64_t pc);
+    void print_source(const std::string& file_name, unsigned line, unsigned n_lines_context = 0);
 };
